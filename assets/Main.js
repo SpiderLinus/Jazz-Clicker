254eea9b56af4ba5897cd4657db274dc07e3c759
spiderlinus <linusschon08@gmail.com>
2025-09-16 19:40:41 +0200

    some changes
    some changes for you

---
+++ assets/Main.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("btn")
+const btn2 = document.getElementById("btn2")
+
+
+
+btn.addEventListener("click", () =>{
+
+    location.href = "mainpage/index.html"
+    
+})
+
+
+
+
+
+btn2.addEventListener("click", () =>{
+
+    location.href = "mainpage2/index.html"
+
+})
+
+
